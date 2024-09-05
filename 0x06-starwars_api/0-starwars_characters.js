160b63b3cdd38c23079cdf6c0f3cc7c182be4206
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-09-05 20:12:59 +0000

    To list Star Wars movie characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,49 @@
+#!/usr/bin/node
+const request = require('request');
+
+// Function to get characters from a specified Star Wars movie
+function getMovieCharacters (movieId) {
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error(`Failed to retrieve data for movie ID ${movieId}:`, error.message);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      console.error(`Failed to retrieve data for movie ID ${movieId}. Status Code: ${response.statusCode}`);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    const characters = data.characters;
+
+    let completedReq = 0;
+
+    characters.forEach(characterUrl => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          console.error('Failed to retrieve character data:', error.message);
+          return;
+        }
+
+        const characterData = JSON.parse(body);
+        console.log(characterData.name);
+
+        completedReq++;
+        if (completedReq === characters.length) {
+          process.exit(0);
+        }
+      });
+    });
+  });
+}
+
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: script.js <Movie ID>');
+  process.exit(1);
+}
+
+getMovieCharacters(movieId);
