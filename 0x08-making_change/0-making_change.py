1167617bc53cd0f255b4d36b255b31420e6564a7
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-09-19 22:02:10 +0000

    To get number of coins needed to make change for a given amount.

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Defines that solves the change making problem. """
+
+
+def makeChange(coins, total):
+    """ Returns the minimum number of coins to make change
+        a particular amount.
+    """
+    if total < 1:
+        return 0
+    mem = [total + 1 for i in range(total + 1)]
+
+    for idx, val in enumerate(mem):
+        if idx == 0:
+            mem[idx] = 0
+        for coin in coins:
+            remain = idx - coin
+            if remain >= 0:
+                mem[idx] = min(mem[remain] + 1, mem[idx])
+    if mem[total] > total:
+        return -1
+    else:
+        return mem[total]
