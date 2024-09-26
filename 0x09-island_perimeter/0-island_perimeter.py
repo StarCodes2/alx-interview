1052637ef83c943750947ad6f470d2be3e838266
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-09-26 17:36:28 +0000

    To find the perimeter of an island represented with a list of list.

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,15 +5,17 @@
 def island_perimeter(grid):
     """ Returns the perimeter of a island represented by a list of lists. """
     perimeter = 0
-    for i in range(len(grid)):
+    row = len(grid)
+    for i in range(row):
         for j in range(len(grid[i])):
+            col = len(grid[i])
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0:
+                if i < 1 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if grid[i + 1][j] == 0:
+                if i >= row or grid[i + 1][j] == 0:
                     perimeter += 1
-                if grid[i][j - 1] == 0:
+                if j < 1 or grid[i][j - 1] == 0:
                     perimeter += 1
-                if grid[i][j + 1] == 0:
+                if j >= col or grid[i][j + 1] == 0:
                     perimeter += 1
     return perimeter
