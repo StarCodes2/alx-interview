8a36cde230101209ebf1c1070e218a3e2530d754
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-10-03 17:15:00 +0000

    FEAT: Find the winner of a prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" Defines a function that finds the winner of a prime game. """
+
+
+def findPrime(numList):
+    """ finds the first prime number in a list. """
+    count = 0
+    for num in numList:
+        for n in range(1, num + 1):
+            if num % n == 0:
+                count += 1
+        if count == 2:
+            return num
+        count = 0
+    return None
+
+
+def isWinner(x, nums):
+    """ Returns the winner of a prime game. """
+    wins = [0, 0]
+    for num in nums:
+        numList = [i for i in range(1, num + 1)]
+        pick = findPrime(numList)
+        track = 1
+
+        while (pick):
+            numList.remove(pick)
+
+            mul = 2
+            while pick * mul in numList:
+                numList.remove(pick * mul)
+                mul += 1
+            else:
+                break
+            track += 1
+            pick = findPrime(numList)
+        if track % 2 == 1:
+            wins[1] += 1
+        else:
+            win[0] += 1
+
+    if wins[0] > wins[1]:
+        return "Maria"
+    elif wins[0] < wins[1]:
+        return "Ben"
+    else:
+        return "Draw"
