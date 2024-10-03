69f7d25058115c916d50cedc7311ada75c682c7c
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-10-03 19:21:48 +0000

    Code Optimized.

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,16 +2,21 @@
 """ Defines a function that finds the winner of a prime game. """
 
 
+def isPrime(n):
+    """Check if a number is prime."""
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
 def findPrime(numList):
     """ finds the first prime number in a list. """
-    count = 0
     for num in numList:
-        for n in range(1, num):
-            if num % n == 0:
-                count += 1
-        if count == 1:
+        if isPrime(num):
             return num
-        count = 0
     return None
 
 
