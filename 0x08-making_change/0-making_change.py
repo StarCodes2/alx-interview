3cfbe8fbd1a5d2c682b41c3248e02ed6916e4137
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-11-26 13:29:34 +0000

    To determine the fewest number of coins needed to meet a given amount total.

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Defines that solves the change making problem. """
+
+
+def makeChange(coins, total):
+    """ Returns the minimum number of coins to make change
+        a particular amount.
+    """
+    if total < 1:
+        return 0
+    mem = [total + 1 for i in range(total + 1)]
+    mem[0] = 0
+
+    for idx, val in enumerate(mem):
+        if idx == 0:
+            mem[idx] = 0
+        for coin in coins:
+            remain = idx - coin
+            if remain >= 0:
+                mem[idx] = min(mem[remain] + 1, mem[idx])
+    return -1 if mem[total] > total else mem[total]
