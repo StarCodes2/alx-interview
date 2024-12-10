78cbd7e529f5ed8deb78ebf2660e5e9ae9e54634
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-12-10 20:17:15 +0000

    The Prime Game

---
+++ 0x0A-primegame/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+isWinner = __import__('0-prime_game').isWinner
+
+nums = [0] * 100
+for i in range(100):
+    nums[i] = i * i
+
+print("Winner: {}".format(isWinner(100, nums)))
